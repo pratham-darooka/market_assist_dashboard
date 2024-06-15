560fcc1cafd9bebae9a4025aea11fcd6bdd7246c
PD <56485898+pratham-darooka@users.noreply.github.com>
2024-06-15 09:38:31 +0000

    concurrent services

--- app/utils/services.py
+++ app/utils/services.py
@@ -2,16 +2,29 @@ from loguru import logger
 import subprocess
 import sys
 import os
+import threading
+
+def run_subprocess(command):
+    process = subprocess.Popen(args=command)
+    logger.info(f"Started subprocess {command} with PID {process.pid}")
 
 def trigger_update_jobs():
     sys.path.append(os.path.join(os.getcwd()))
 
-    p1 = subprocess.Popen(args=["python", "jobs/update_stocks.py"])
-    logger.info(f"triggered stock update services {p1.communicate()}")
-    p2 = subprocess.Popen(args=["python", "jobs/update_indices.py"])
-    logger.info(f"triggered index update services {p2.communicate()}")
-    p3 = subprocess.Popen(args=["python", "jobs/update_mc.py"])
-    logger.info(f"triggered mc update services {p3.communicate()}")
+    commands = [
+        ["python", "jobs/update_indices.py"],
+        ["python", "jobs/update_stocks.py"],
+        ["python", "jobs/update_mc.py"]
+    ]
+
+    threads = []
+    for command in commands:
+        thread = threading.Thread(target=run_subprocess, args=(command,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == "__main__":
